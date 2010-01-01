a96653940040e2ff2203b229d14b5a9fb278613a
pregame <jostster@gmail.com>
2010-01-01 01:52:59 -0700

    removed .sh scripts from folder and added donation link

--- app/scripts/lightoff.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo -n shutdown >/sys/class/i2c-adapter/i2c-2/2-0033/mode
-echo -n 0mA >/sys/class/i2c-adapter/i2c-2/2-0033/torch_current
-echo -n 0 >/sys/class/i2c-adapter/i2c-2/2-0033/avin
-
--- app/scripts/lighton.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo -n 1 >/sys/class/i2c-adapter/i2c-2/2-0033/avin
-echo -n 100mA >/sys/class/i2c-adapter/i2c-2/2-0033/torch_current
-echo -n torch >/sys/class/i2c-adapter/i2c-2/2-0033/mode
-
-
